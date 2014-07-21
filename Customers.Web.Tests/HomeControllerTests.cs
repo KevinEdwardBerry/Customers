e8a744bea48e705fe3e693dcf12fd354016308ce
KevinEdwardBerry <kevin.berry@improvingenterprises.com>
2014-07-21 14:00:09 -0500

    Re-added the top nav bar and a simple test for the HomeController

---
+++ Customers.Web.Tests/HomeControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Customers.Web.Controllers;
+
+namespace Customers.Web.Tests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void When_Viewing_The_Home_Page_The_Controler_Should_Return_The_Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
