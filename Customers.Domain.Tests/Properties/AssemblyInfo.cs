567dcb4c27c47c6772cdd7819cb3a113d5e3c4a9
KevinEdwardBerry <kevin.berry@improvingenterprises.com>
2014-07-21 20:40:12 -0500

    Re-added Domain and Tests projects to see if Azure still works

--- Customers.Domain.Tests/Properties/AssemblyInfo.cs
+++ Customers.Domain.Tests/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("70607cc0-2503-425a-a0f0-ed6da272da02")]
+[assembly: Guid("e74971b6-96a7-4e6a-be7e-675231baaf6b")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Customers.Domain/Properties/AssemblyInfo.cs
+++ Customers.Domain/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("904a093a-adc2-4372-b1c1-5efe03fee86b")]
+[assembly: Guid("f0274c08-d25f-4dd0-9084-ba68b66f5b4e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
