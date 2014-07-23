5f68cf76a7b2a3535a6c5c788938e2e1ff8bc479
KevinEdwardBerry <kevin.berry@improvingenterprises.com>
2014-07-23 18:33:39 -0500

    WIP

---
+++ Customers.Web.Tests/When_Accessing_The_Database_From_The_Repo.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customers.Web.Tests
+{
+    [TestFixture]
+    public class When_Accessing_The_Database_From_The_Repo
+    {
+        //
+    }
+}
