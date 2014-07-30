c89c094f97ae09f75fcfa700adef7417a95cc3be
KevinEdwardBerry <kevin.berry@improvingenterprises.com>
2014-07-30 08:13:57 -0500

    Changed error message settings and refactored a test file

--- Customers.Web.Tests/HomeControllerTests.cs
+++ Customers.Web.Tests/HomeControllerTests.cs
@@ -15,13 +15,18 @@ namespace Customers.Web.Tests
     [TestFixture]
     public class HomeControllerTests
     {
+        private Repository repo;
+        private HomeController controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            repo = new Repository(new InMemoryDataContext());
+            controller = new HomeController(repo);
+        }
         [Test]
         public void When_Viewing_The_Home_Page_The_Controler_Should_Return_The_Index()
         {
-            // Arrange
-            var repo = new Repository(new InMemoryDataContext());
-            HomeController controller = new HomeController(repo);
-
             // Act
             ViewResult result = controller.Index() as ViewResult;
 
@@ -32,8 +37,6 @@ namespace Customers.Web.Tests
         [Test]
         public void After_Creating_A_New_Customer_A_Confirmation_Should_Appear()
         {
-            var repo = new Repository(new InMemoryDataContext());
-            HomeController controller = new HomeController(repo);
             var result = controller.NewCustomerConfirmation() as ViewResult;
             Assert.IsNotNull(result);
         }
@@ -41,8 +44,6 @@ namespace Customers.Web.Tests
         [Test]
         public void When_Deleting_A_Customer_From_The_Database_You_Should_Be_Returned_To_The_Index()
         {
-            var repo = new Repository(new InMemoryDataContext());
-            HomeController controller = new HomeController(repo);
             var result = controller.DeleteCurrentCustomer(0) as ViewResult;
             Assert.IsNotNull(result);
             Assert.AreEqual("Index", result.ViewName);
@@ -51,8 +52,6 @@ namespace Customers.Web.Tests
         [Test]
         public void When_Editing_A_Customer_From_The_Database_You_Should_Be_Returned_To_The_Index()
         {
-            var repo = new Repository(new InMemoryDataContext());
-            HomeController controller = new HomeController(repo); ;
             var result = controller.EditCurrentCustomer(0) as ViewResult;
             Assert.IsNotNull(result);
             Assert.AreEqual("Index", result.ViewName);
